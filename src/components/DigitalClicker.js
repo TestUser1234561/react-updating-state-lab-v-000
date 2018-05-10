9dd79fbd90244b532c1248bcb3e4364765e5aba3
Morgan Fudge <mfudge92@gmail.com>
2018-05-10 17:02:46 +0000

    automatically backed up by learn

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -11,7 +11,7 @@ export default class DigitalClicker extends React.Component {
 
   handleClick = () => {
     this.click({
-      timesClicked: ++this.state.timesClicked,
+      timesClicked: this.state.timesClicked++,
     });
   }
 
